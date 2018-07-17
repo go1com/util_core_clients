f356c6cacf39f8678c92938365cb03e5dd11e136
Andy Truong <thehongtt@gmail.com>
2018-07-17 15:02:01 +0700

    update

--- tests/UtilClientTestCase.php
+++ tests/UtilClientTestCase.php
@@ -2,10 +2,13 @@
 
 namespace go1\clients\tests;
 
+use Doctrine\Common\Cache\ArrayCache;
 use go1\clients\MqClient;
 use go1\clients\UtilCoreClientServiceProvider;
+use go1\util\Service;
 use go1\util\tests\QueueMockTrait;
 use go1\util\tests\UtilCoreTestCase;
+use GuzzleHttp\Client;
 use Pimple\Container;
 
 class UtilClientTestCase extends UtilCoreTestCase
@@ -27,7 +30,16 @@ class UtilClientTestCase extends UtilCoreTestCase
     protected function getContainer(): Container
     {
         $c = parent::getContainer();
-        $c->register(new UtilCoreClientServiceProvider);
+        $c['client'] = new Client;
+        $c['cache'] = new ArrayCache;
+        $c->register(new UtilCoreClientServiceProvider, [
+            'queueOptions' => [
+                    'host' => '172.31.11.129',
+                    'port' => '5672',
+                    'user' => 'go1',
+                    'pass' => 'go1',
+                ] + Service::urls(['queue', 'user', 'mail', 'portal', 'rules', 'currency', 'lo', 'sms', 'graphin', 's3', 'realtime'], 'qa'),
+        ]);
 
         return $c;
     }
