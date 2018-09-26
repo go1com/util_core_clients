e9e5f5b933d548df96f91c1655aa364bcbd86d06
salvatore.leotta@go1.com <salvatore.leotta@go1.com>
2018-09-26 13:59:22 +0800

    GO1P-21333 adding s3 video client

--- UtilCoreClientServiceProvider.php
+++ UtilCoreClientServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace go1\clients;
 
+use Aws\Credentials\Credentials;
+use Aws\S3\S3Client;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 
@@ -27,6 +29,22 @@ class UtilCoreClientServiceProvider implements ServiceProviderInterface
 
         $c['go1.client.explore'] = function (Container $c) {
             return new ExploreClient($c['client'], $c['explore_url']);
+        };
+
+        $c['go1.client.s3Video'] = function (Container $c) {
+            $o = $c['videoS3Options'];
+            $args = [
+                'region'      => $o['region'],
+                'version'     => $o['version'],
+                'credentials' => new Credentials($o['key'], $o['secret']),
+            ];
+            if (getenv('MONOLITH')) {
+                // https://github.com/minio/cookbook/blob/master/docs/aws-sdk-for-php-with-minio.md
+                $args['endpoint'] = $o['endpoint'];
+                $args['use_path_style_endpoint'] = true;
+            }
+
+            return new S3Client($args);
         };
 
         $c['go1.client.mq'] = function (Container $c) {
